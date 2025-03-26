8c92071e6fc177ca0981c65622fe186a88d7c70e
Sparsh Doshi <123226042+sparsh337@users.noreply.github.com>
2025-03-26 14:01:43 +0530

    Updated Multer to use Cloudinary & configured env variables

--- routes/multer.js
+++ routes/multer.js
@@ -1,17 +1,25 @@
 const multer = require("multer");
-const path = require("path");
-const crypto = require("crypto");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+require("dotenv").config();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images/uploads");
-  },
-  filename: function (req, file, cb) {
-    const fn =
-      crypto.randomBytes(16).toString("hex") + path.extname(file.originalname);
-    cb(null, fn);
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Configure Multer to use Cloudinary Storage
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "instavibe/uploads", // Change the folder name as needed
+    allowed_formats: ["jpeg", "png", "jpg"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }],
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
+
 module.exports = upload;
